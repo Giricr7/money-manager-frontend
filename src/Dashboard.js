f61b291c748043ad2ec75b5cc4db929663b4a71d
Giriprasath <giriarjun1995@gmail.com>
2021-11-24 17:56:13 -0700

    array reversed

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -30,10 +30,13 @@ const url='https://money-manager-backend-srvr.herokuapp.com'
          (async() => {
              const income = await axios.get(`${url}/income`);
              const expense = await axios.get(`${url}/expense`);
-             setIncomeData((income.data).reverse());
-             setExpenseData((expense.data).reverse());
-             setFetchedIncome((income.data).reverse());
-             setFetchedExpense((expense.data).reverse());
+             income.data=income.data.reverse()
+             expense.data = expense.data.reverse()
+             setIncomeData(income.data);
+             setExpenseData(expense.data);
+             setFetchedIncome(income.data);
+             setFetchedExpense(expense.data);
+
          })();
          
      }, [])
