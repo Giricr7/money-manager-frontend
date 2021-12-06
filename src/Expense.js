0978811febd56aef42953a596f2628c714c8c81d
Giriprasath <giriarjun1995@gmail.com>
2021-12-06 09:17:31 -0700

    adding loading screens

--- src/Expense.js
+++ src/Expense.js
@@ -2,8 +2,11 @@ import { Expenses } from './custStyle'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
 import { useState } from 'react'
+import { RingLoader } from "react-spinners"
+import { css } from "@emotion/react";
 import swal from 'sweetalert'
 const url = 'https://money-manager-backend-srvr.herokuapp.com/expense'
+
 
 function Expense() {
 
@@ -14,6 +17,16 @@ function Expense() {
     let Day = new Date().toLocaleDateString();
     let Time = new Date().toLocaleTimeString();
     let expTime = Date.now() + 43200000;
+    let [loading, setLoading] = useState(false);
+
+    const override = css`
+    display: block;
+    margin: 0 auto;
+    border-color: red;
+    direction:column;
+    justify-content:center;
+    align-items:center;
+  `;
 
     function resetData()
     {
@@ -25,15 +38,18 @@ function Expense() {
 
     let submitData = async (e) => {
         e.preventDefault();
+        setLoading(true)
         try {
             if (Type!=='' && Amount!=='' && Description!=='' && Division!=='') {
                 const res = await axios.post(url, { Type, Amount, Description,Division, Day,Time,expTime});
-            if(res)
+                if (res) {
+                    setLoading(false)
                     swal("Success!", "Data Added Successfully!", "success");
                     resetData()
                     e.target.reset();
-               
+                }
             } else {
+                setLoading(false)
                 swal("Error!","Fill the required fields", "warning");
             }
             }catch (err) {
@@ -45,67 +61,67 @@ function Expense() {
     return (
         
         <>
-
-<div className="expense-div">
-                <h1>ADD NEW EXPENSE</h1>
-                <hr></hr>
-                <div className="card expense-items">
-                <div >
-                        <form onSubmit={submitData}>
+            {loading ? <div className="App"> <RingLoader color={"blue"} loading={loading} css={override} size={60} /></div> :
+                <div className="expense-div">
+                    <h1>ADD NEW EXPENSE</h1>
+                    <hr></hr>
+                    <div className="card expense-items">
+                        <div >
+                            <form onSubmit={submitData}>
                             
-                        <label>Division</label><br></br>
-                        <select onChange={(e)=>{setDivision(e.target.value)}}>
-                            <option>Personal Expense</option>
-                            <option>Official Expense</option>
-                        </select><br></br><br></br><br></br>
+                                <label>Division</label><br></br>
+                                <select onChange={(e) => { setDivision(e.target.value) }}>
+                                    <option>Personal Expense</option>
+                                    <option>Official Expense</option>
+                                </select><br></br><br></br><br></br>
 
-                        <label>Type</label><br></br>
-                        <select onChange={(e)=>{setType(e.target.value)}}>
-                                {
-                                    Expenses.map((exp)=><option>{exp}</option>)
-                                 }
-                        </select><br></br><br></br><br></br>
+                                <label>Type</label><br></br>
+                                <select onChange={(e) => { setType(e.target.value) }}>
+                                    {
+                                        Expenses.map((exp) => <option>{exp}</option>)
+                                    }
+                                </select><br></br><br></br><br></br>
                         
-                        <label>Enter the Amount in-(Rs.)</label><br></br>
-                            <input
-                                className='exp-inp'
-                                type='number'
-                                onChange={(e)=>{setAmount(e.target.value)}}
-                                onInput = {(e) =>{
-                                e.target.value = Math.max(0, parseInt(e.target.value) ).toString().slice(0,12)
-                                }}
-                        ></input><br></br><br></br><br></br>
+                                <label>Enter the Amount in-(Rs.)</label><br></br>
+                                <input
+                                    className='exp-inp'
+                                    type='number'
+                                    onChange={(e) => { setAmount(e.target.value) }}
+                                    onInput={(e) => {
+                                        e.target.value = Math.max(0, parseInt(e.target.value)).toString().slice(0, 12)
+                                    }}
+                                ></input><br></br><br></br><br></br>
                         
-                        <label>Description</label><br></br>
-                            <textarea
-                                className='exp-inp'
-                                type='text'
-                                onChange={(e)=>{setDescription(e.target.value)}}
-                                maxLength={100}
+                                <label>Description</label><br></br>
+                                <textarea
+                                    className='exp-inp'
+                                    type='text'
+                                    onChange={(e) => { setDescription(e.target.value) }}
+                                    maxLength={100}
                                 ></textarea><br></br><br></br><br></br>
                             
-                            <label>Transaction Date & Time</label>
+                                <label>Transaction Date & Time</label>
 
-                            <p >{Day}-{Time }</p><br></br><br></br>
+                                <p >{Day}-{Time}</p><br></br><br></br>
 
 
-                            <input className='btn btn-success' type='submit' value='Add Transaction' ></input>
-                            &nbsp;&nbsp;
+                                <input className='btn btn-success' type='submit' value='Add Transaction' ></input>
+                                &nbsp;&nbsp;
                                 <Link to='/income' >Go to Income</Link>
-                            <br></br><br></br><br></br>
+                                <br></br><br></br><br></br>
                             
                             
 
-                        </form>
+                            </form>
                     
-                </div>
-                </div>
+                        </div>
+                    </div>
                 
                 
 
-          </div>  
+                </div>
 
-
+            }
         </>
     )
 }
--- src/Income.js
+++ src/Income.js
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom'
 import axios from 'axios'
 import { useState } from 'react'
 import swal from 'sweetalert'
+import { RingLoader } from "react-spinners"
+import { css } from "@emotion/react";
 
 const url='https://money-manager-backend-srvr.herokuapp.com/income'
 
@@ -14,7 +16,17 @@ function Income() {
     let Day = new Date().toLocaleDateString();
     let Time = new Date().toLocaleTimeString();
     let expTime = Date.now() + 43200000;
+    let [loading, setLoading] = useState(false);  
     
+    const override = css`
+    display: block;
+    margin: 0 auto;
+    border-color: red;
+    direction:column;
+    justify-content:center;
+    align-items:center;
+  `;
+
     function resetData()
     {
         setType('Monthly Income');
@@ -24,19 +36,22 @@ function Income() {
    
     let submitData = async (e) => {
         e.preventDefault();
+        setLoading(true)
         try {
             if (Type!=='' && Amount!=='' && Description!=='')
             {
                 const res = await axios.post(url, { Type, Amount, Description, Day,Time,expTime });
-                console.log(res);
+                
                 if (res) {
+                    setLoading(false)
                     swal("Success!", "Data Added Successfully!", "success");
                     resetData()
                     e.target.reset();
                 }
               
                 
-            }else {
+            } else {
+                setLoading(false)
                 swal("Error!","Fill the required fields", "warning");
             }
             }catch (err) {
@@ -48,63 +63,65 @@ function Income() {
     
     return (
     
-        <>
+        <div >
+            {
+               loading?<div className="App"> <RingLoader color={"blue"} loading={loading} css={override} size={60} /></div>:
 
-            <div className="income-div">
-                <h1>ADD NEW INCOME</h1>
-                <hr></hr>
-                <div className="card income-items">
-                <div >
-                        <form onSubmit={submitData }>
+                <div className="income-div">
+                    <h1>ADD NEW INCOME</h1>
+                    <hr></hr>
+                    <div className="card income-items">
+                        <div >
+                            <form onSubmit={submitData}>
 
-                        <label>Type</label><br></br>
-                        <select onChange={(e)=>{setType(e.target.value)}}>
-                            <option>Monthly Income</option>
-                            <option>Business Income</option>
-                        </select><br></br><br></br><br></br>
+                                <label>Type</label><br></br>
+                                <select onChange={(e) => { setType(e.target.value) }}>
+                                    <option>Monthly Income</option>
+                                    <option>Business Income</option>
+                                </select><br></br><br></br><br></br>
                         
-                        <label>Enter the Amount in-(Rs.)</label><br></br>
-                            <input
-                                className='inc-inp'
-                                type='number'
-                                onChange={(e)=>{setAmount(e.target.value)}}
-                                onInput = {(e) =>{
-                                e.target.value = Math.max(0, parseInt(e.target.value) ).toString().slice(0,12)
-                                }}
-                        ></input><br></br><br></br><br></br>
+                                <label>Enter the Amount in-(Rs.)</label><br></br>
+                                <input
+                                    className='inc-inp'
+                                    type='number'
+                                    onChange={(e) => { setAmount(e.target.value) }}
+                                    onInput={(e) => {
+                                        e.target.value = Math.max(0, parseInt(e.target.value)).toString().slice(0, 12)
+                                    }}
+                                ></input><br></br><br></br><br></br>
                         
-                        <label>Description</label><br></br>
-                            <textarea
-                                className='inc-inp'
-                                type='text'
-                                maxLength={100}
-                                onChange={(e)=>{setDescription(e.target.value)}}
+                                <label>Description</label><br></br>
+                                <textarea
+                                    className='inc-inp'
+                                    type='text'
+                                    maxLength={100}
+                                    onChange={(e) => { setDescription(e.target.value) }}
                                 ></textarea><br></br><br></br><br></br>
                             
-                            <label>Transaction Date & Time</label>
+                                <label>Transaction Date & Time</label>
 
-                            <p id='date'>{Day}-{Time }</p><br></br><br></br>
+                                <p id='date'>{Day}-{Time}</p><br></br><br></br>
 
 
-                            <input className='btn btn-success' type='submit' value='Add Transaction' ></input>
-                            &nbsp;&nbsp;
+                                <input className='btn btn-success' type='submit' value='Add Transaction' ></input>
+                                &nbsp;&nbsp;
                                 <Link to='/expense' >Go to Expense</Link>
-                            <br></br><br></br><br></br>
+                                <br></br><br></br><br></br>
                             
                            
 
-                        </form>
+                            </form>
                     
-                </div>
-                </div>
+                        </div>
+                    </div>
                 
                 
 
-          </div>  
+                </div>
 
+            }
 
-
-        </>
+        </div>
             )
 
 
